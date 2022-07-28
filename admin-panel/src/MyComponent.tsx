e4cf8a87ea3d8a0b057ba4fb1c6b9fee45a1833f
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-07-28 07:58:42 +0200

    extend with try feature

--- admin-panel/src/MyComponent.tsx
+++ admin-panel/src/MyComponent.tsx
@@ -2,15 +2,13 @@ import React, { useState, useEffect } from "react";
 import {
 	Button,
 	TextControl,
-	TabPanel,
+	PanelBody,
 	SelectControl,
+	TextareaControl,
+	Panel,
 } from "@wordpress/components";
 
 declare var wpApiSettings: { nonce: string };
-const wrapperStyle: any = {
-	display: "flex",
-	flexDirection: "column",
-};
 
 const methods = ["GET", "POST", "PUT", "DELETE"].map((i) => ({
 	label: i,
@@ -19,6 +17,10 @@ const methods = ["GET", "POST", "PUT", "DELETE"].map((i) => ({
 
 export default function MyComponent() {
 	const [items, setItems] = useState([]);
+	const [isOpen, setOpen] = useState(true);
+	const [tryResponse, setResponse] = useState("");
+
+	const [reqBody, setReqBody] = useState("");
 	const [name, setName] = useState("");
 	const [url, setUrl] = useState("");
 	const [method, setMethod] = useState("GET");
@@ -55,7 +57,31 @@ export default function MyComponent() {
 		});
 		loadContent();
 		setName("");
+		setMethod("GET");
 		setUrl("");
+		setReqBody("");
+		setResponse("");
+	};
+
+	const chooseItem = (id: number) => {
+		const { name, url, method }: any = items.filter((i) => i.id === id)[0];
+
+		setName(name);
+		setUrl(url);
+		setMethod(method);
+	};
+
+	const tryAction = async () => {
+		const headers = reqBody ? { "Content-Type": "application/json" } : {};
+
+		const res = await fetch(url, {
+			method,
+			headers,
+			body: reqBody ? reqBody : undefined,
+		});
+
+		const result = await res.text();
+		setResponse(result);
 	};
 
 	useEffect(() => {
@@ -63,81 +89,95 @@ export default function MyComponent() {
 	}, []);
 
 	return (
-		<TabPanel
-			tabs={[
-				{
-					name: "tab1",
-					title: "Datasources",
-				},
-				{
-					name: "tab2",
-					title: "Add new one",
-				},
-			]}
-		>
-			{(tab) => {
-				switch (tab.name) {
-					case "tab1":
-						return (
-							<div className="wrap">
-								<table className="wp-list-table widefat fixed striped table-view-list">
-									<thead>
-										<tr>
-											<th>Id</th>
-											<th>Name</th>
-											<th>Method</th>
-											<th>Url</th>
-											<th></th>
-										</tr>
-									</thead>
+		<div className="wrap">
+			<Panel header="Inseri Panel">
+				<PanelBody
+					title={"Add Datasource"}
+					opened={isOpen}
+					onToggle={() => setOpen(!isOpen)}
+				>
+					<div
+						style={{
+							margin: "12px 0",
+						}}
+					>
+						<div
+							style={{
+								display: "inline",
+								padding: "0 10px 0 0",
+							}}
+						>
+							<Button variant="primary" onClick={insertContent}>
+								Insert
+							</Button>
+						</div>
+						<Button variant="primary" onClick={tryAction}>
+							Try
+						</Button>
+					</div>
 
-									<tbody>
-										{items.map(({ id, name, url, method }) => (
-											<tr key={id}>
-												<td>{id}</td>
-												<td>{name}</td>
-												<td>{method}</td>
-												<td>{url}</td>
-												<td>
-													<Button
-														onClick={() => deleteItem(id)}
-														variant="secondary"
-														isSmall
-													>
-														Delete
-													</Button>
-												</td>
-											</tr>
-										))}
-									</tbody>
-								</table>
-							</div>
-						);
+					<TextControl
+						label="Datasource Name"
+						onChange={setName}
+						value={name}
+					/>
+					<SelectControl
+						label="HTTP Method"
+						onChange={setMethod as any}
+						value={method ? method : "GET"}
+						options={methods}
+					/>
+					<TextControl label="URL" onChange={setUrl} value={url} />
+					<TextareaControl
+						label="Request Body"
+						value={reqBody}
+						onChange={setReqBody}
+					/>
+					<TextareaControl
+						label="Response"
+						value={tryResponse}
+						onChange={null}
+					/>
+				</PanelBody>
 
-					case "tab2":
-						return (
-							<div className="wrap">
-								<div style={{ margin: "8px" }}>
-									{" "}
-									<TextControl
-										label="Datasource Name"
-										onChange={setName}
-										value={name}
-									/>
-									<SelectControl
-										label="HTTP Method"
-										onChange={setMethod as any}
-										options={methods}
-									/>
-									<TextControl label="URL" onChange={setUrl} value={url} />
-									<Button variant="primary" onClick={insertContent}>
-										Insert
-									</Button>
-								</div>
-							</div>
-						);
-				}
-			}}
-		</TabPanel>
+				<PanelBody>
+					<table className="wp-list-table widefat fixed striped table-view-list">
+						<thead>
+							<tr>
+								<th style={{ width: "5%" }}>Id</th>
+								<th>Name</th>
+								<th style={{ width: "5%" }}>Method</th>
+								<th>Url</th>
+								<th></th>
+							</tr>
+						</thead>
+
+						<tbody>
+							{items.map(({ id, name, url, method }) => (
+								<tr
+									key={id}
+									onClick={() => chooseItem(id)}
+									style={{ cursor: "pointer" }}
+								>
+									<td>{id}</td>
+									<td className="row-title">{name}</td>
+									<td>{method}</td>
+									<td>{url}</td>
+									<td>
+										<Button
+											onClick={() => deleteItem(id)}
+											variant="secondary"
+											isSmall
+										>
+											Delete
+										</Button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</PanelBody>
+			</Panel>
+		</div>
 	);
 }
